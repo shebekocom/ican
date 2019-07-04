f6ed59fda25f910e76362bbe0031a8e94862bb6c
nshebeko <nikolay.shebeko@gmail.com>
2019-07-04 15:53:31 +0300

    made a checkbox selection when clicking on a label

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const selectAllButton = document.getElementById('test')
-const inputElement = document.getElementById('input')
-const ulElement = document.getElementById('list')
+const selectAllButton = document.getElementById('test');
+const inputElement = document.getElementById('input');
+const ulElement = document.getElementById('list');
 
 const todoList = []
 //upgradeView()
 
 
 selectAllButton.addEventListener('click', () => {
-    console.log('fired')
-})
+    console.log('fired');
+});
 
 inputElement.addEventListener('keydown', event => {
     if (event.key === 'Enter' || event.keyCode === 13) {
@@ -16,55 +16,58 @@ inputElement.addEventListener('keydown', event => {
             content: inputElement.value,
             done: false
         })
-        inputElement.value = ''
+        inputElement.value = '';
 
-        upgradeView()
+        upgradeView();
     }
     
 })
 
 
 function upgradeView () {
-    ulElement.innerHTML = ''
+    ulElement.innerHTML = '';
 
-    for (const todoItem of todoList) {
-        const liElement = document.createElement('li')
-        liElement.className = 'list-group-item'
-        ulElement.append(liElement)
+    for (let index = 0; index < todoList.length; index++) {
+        const todoItem = todoList[index];
+        
+        const liElement = document.createElement('li');
+        liElement.className = 'list-group-item';
+        ulElement.append(liElement);
 
-        const divElement = document.createElement('div')
-        divElement.className = 'form-group form-check'
-        liElement.append(divElement)
+        const divElement = document.createElement('div');
+        divElement.className = 'form-group form-check';
+        liElement.append(divElement);
 
-        const checkboxElement = document.createElement('input')
-        divElement.append(checkboxElement)
-        checkboxElement.type = 'checkbox'
-        checkboxElement.className = 'form-check-input'
+        const checkboxElement = document.createElement('input');
+        divElement.append(checkboxElement);
+        checkboxElement.type = 'checkbox';
+        checkboxElement.className = 'form-check-input';
+        checkboxElement.id = 'todoItem' + index;
 
-        const labelElement = document.createElement('label')
+        const labelElement = document.createElement('label');
         divElement.append(labelElement)
-        labelElement.className = 'form-check-label'
+        labelElement.className = 'form-check-label';
         if (todoItem.done) {
-            labelElement.className = ' todoDone'
+            labelElement.className == ' todoDone';
         }
-        labelElement.setAttribute = ('for', 'exampleCheck1')
-        labelElement.innerText = todoItem.conent
+        labelElement.setAttribute ('for', 'todoItem' + index);
+        labelElement.innerText = todoItem.content;
 
-        const buttonDoneElement = document.createElement('button')
-        divElement.append(buttgionDoneElement)
-        buttonDoneElement.type = 'button'
-        buttonDoneElement.className = 'btn btn-outline-primary'
-        buttonDoneElement.innerText = 'Done'
+        const buttonDoneElement = document.createElement('button');
+        divElement.append(buttonDoneElement);
+        buttonDoneElement.type = 'button';
+        buttonDoneElement.className = 'btn btn-outline-primary';
+        buttonDoneElement.innerText = 'Done';
 
-        const buttonRemoveElement = document.createElement('button')
-        divElement.append(buttonRemoveElement)
-        buttonRemoveElement.type = 'button'
-        buttonRemoveElement.className = 'btn btn-outline-danger'
-        buttonRemoveElement.innerText = 'Remove'
+        const buttonRemoveElement = document.createElement('button');
+        divElement.append(buttonRemoveElement);
+        buttonRemoveElement.type = 'button';
+        buttonRemoveElement.className = 'btn btn-outline-danger';
+        buttonRemoveElement.innerText = 'Remove';
 
         buttonDoneElement.addEventListener('click', () => {
-            todoItem.done=!todoItem.done
-            upgradeView()
-        })
+            todoItem.done=!todoItem.done;
+            upgradeView();
+        });
     }
 }
